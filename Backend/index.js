ede3fc841333d9b7ce860c822bf0f3db4a73db29
samarthsinh2660 <rajuvala80@gmail.com>
2025-10-31 00:26:00 +0530

    fix cors

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ app.use(limiter);
 // CORS configuration
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://front-affirmations.netlify.app','https://affirmations-amber.vercel.app/','http://localhost:5173','http://localhost:3000'] 
+    ? ['https://front-affirmations.netlify.app','https://affirmations-amber.vercel.app','http://localhost:5173','http://localhost:3000'] 
     : ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true
 }));
